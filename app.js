3a4ab193fcddb6482c31ded4e6310f3b492247e5
UtechG17 <127623759+Utech17@users.noreply.github.com>
2024-11-04 18:08:32 -0400

    listo

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-const colors = require('colors');
 console.log("Bienvenidos a la primera practica")
-const uniforme = (x,k)=>{
-
-    let prob= Number((x/k).toFixed(4))
-    return {prob}
-}
-const suma =(a,b) =>{
-
-    return a+b
-}
-
 
 console.log("Hola")
 console.log("Este console es para mostrar el uso del push")
-
-console.log(`La probabilidad calculada es: `+`${uniforme(3,6).prob}`.green)
